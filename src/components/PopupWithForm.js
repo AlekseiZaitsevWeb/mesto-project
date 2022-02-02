ab145b837f7b588d334d3b2ced814e69a3512787
Андрей <andrew.milyutin@gmail.com>
2022-02-02 19:46:57 +0300

    added PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,30 @@
+import Popup from "./Popup.js";
+export default class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
+    this._form = this._popupElement.querySelector('.popup__form');
+    this._submitButton = this._popupElement.querySelector('.popup__button-submit');
+    this._buttonDefaultText = this._submitButton.textContent;
+    this._handleFormSubmit = handleFormSubmit;
+  }
+  _getInputValues() {
+    this._inputList = this._popupElement.querySelectorAll('.popup__input-text');
+    this._formValues = {};
+    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+    return this._formValues;
+  }
+  setEventListeners() {
+    this._popupElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    })
+    super._setEventListeners();
+  }
+  close() {
+    this._form.reset();
+    super.close();
+  }
+  renderLoading(nameButton = this._buttonDefaultText) {
+    this._submitButton.textContent = nameButton;
+  }
+}
