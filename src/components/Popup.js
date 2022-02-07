d85c27e0e3a5c8cbd693c402ba11bc51b1c32713
Андрей Милютин <andrew.milyutin@gmail.com>
2022-02-07 16:18:37 +0300

    fix popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,26 +7,20 @@ export default class Popup {
     this._handleClose = this._handleClose.bind(this);
   }
   open() {
+    document.addEventListener("keydown", this._handleClose);
     this._popupElement.classList.add("popup_opened");
-    this._setEventListeners();
   }
   close() {
+    document.removeEventListener("keydown", this._handleClose);
     this._popupElement.classList.remove("popup_opened");
-    this._removeEventListeners();
   }
   _handleClose(evt) {
     if (evt.key === this._escape || evt.target === this._popupElement || evt.target === this._closePopupButton) {
       this.close();
     }
   }
-  _setEventListeners() {
+  setEventListeners() {
     this._popupElement.addEventListener("click", this._handleClose);
-    document.addEventListener("keydown", this._handleClose);
     this._closePopupButton.addEventListener("click", this._handleClose);
-  }
-  _removeEventListeners() {
-    this._popupElement.removeEventListener("click", this._handleClose);
-    document.removeEventListener("keydown", this._handleClose);
-    this._closePopupButton.removeEventListener("click", this._handleClose);
   }
 }
